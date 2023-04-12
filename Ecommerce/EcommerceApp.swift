67be36b849d8fb333a7d9c68ccbb8bbc7d4293d4
Mohan <mohan@hansaitechnosoft.com>
2023-04-12 15:34:17 +0530

    Initial Commit

---
+++ Ecommerce/EcommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceApp.swift
+//  Ecommerce
+//
+//  Created by Diwakar Reddy  on 12/04/23.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
